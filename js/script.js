8526a5e77b379588ac4649f26f014d23b8f5a11b
https://github.com/portfolioasad/carepoint.git <portfolioasad29@gmail.com>
2023-12-30 12:39:56 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.clint_Slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow:'.prev_arr',
+        nextArrow:'.next_arr'
+  });
+});
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nicolafranchini/venobox' => array(
+            'pretty_version' => '2.1.3',
+            'version' => '2.1.3.0',
+            'reference' => '101145aff4c155fd161c2cf1cdb1dc5ffb23d677',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nicolafranchini/venobox',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
